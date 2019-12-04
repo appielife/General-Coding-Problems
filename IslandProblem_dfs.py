161751338ce4e9c3430ca13c1c3bcfa6c0ebd68b
Arpit Parwal <aparwal@usc.edu>
2019-12-03 16:13:42 -0800

    No of island problem

---
+++ IslandProblem_dfs.py
@@ -0,0 +1,50 @@
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+#  An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+# You may assume all four edges of the grid are all surrounded by water.
+# Example 1:
+
+# Input:
+# 11110
+# 11010
+# 11000
+# 00000
+
+# Output: 1
+# Example 2:
+
+# Input:
+# 11000
+# 11000
+# 00100
+# 00011
+
+# Output: 3
+
+
+
+class IslandProblem_dfs(object):
+    def numIslands(self, grid):
+#         return if grid is empty
+        if len(grid) == 0:
+            return 0
+        
+        result=0
+        for i, i_row in enumerate(grid):
+            for j, value in enumerate(i_row):            
+                if value == '1':
+                    result+= self.dfs(grid, i,j)
+        return result
+    
+    
+    def dfs(self, grid, i, j ):
+        if(i<0 or j<0 or i >=len(grid) or j >=len(grid[i]) or grid[i][j] == '0' ):
+            return 0
+        
+        grid[i][j]='0'
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i+1,j)
+        self.dfs(grid, i , j-1)
+        self.dfs(grid, i ,j+1)
+        return 1   
+                
+        
